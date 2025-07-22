1298ea25b6b439014a2b6e0a01cb49b26d2f49e1
Mike Tobia <michael.e.tobia@nasa.gov>
2025-07-22 14:18:50 -0500

    removed mock dbc nodes from pb.launch.py, if we're running a sim of a robot I don't think we'll ever want to run the GUI too

--- launch/pb.launch.py
+++ launch/pb.launch.py
@@ -10,15 +10,8 @@ from ament_index_python.packages import get_package_share_directory
 from drt_ur_gui import SERVICES
 
 def generate_launch_description():
+    
     declared_arguments = []
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "use_fake_hardware",
-            default_value="false",
-            choices=['true', 'false'],
-            description="Start robot with simulated hardware mirroring command to its states"
-        )
-    )
     declared_arguments.append(
         DeclareLaunchArgument(
             "ns",
@@ -26,15 +19,13 @@ def generate_launch_description():
             description="Namespace for the hardware robot"
         )
     )
-    use_fake_hardware = LaunchConfiguration("use_fake_hardware")
     ns = LaunchConfiguration("ns")
-
+    
     config_right = os.path.join(
         get_package_share_directory('drt_ur_gui'),
         'config',
         'pb_right.yaml'
     )
-
     config_left = os.path.join(
         get_package_share_directory('drt_ur_gui'),
         'config',
@@ -57,20 +48,10 @@ def generate_launch_description():
         namespace = ns,
         parameters = [config_left]
     )
-    right_mock_dbc_node = Node(
-        package= "drt_ur_gui",
-        executable="run_mock_dbc.py",
-        name="right_dashboard_client",
-        output='screen',
-        namespace = ns,
-        condition=IfCondition(use_fake_hardware)
-    )
-    left_mock_dbc_node = Node(
-        package= "drt_ur_gui",
-        executable="run_mock_dbc.py",
-        name="left_dashboard_client",
-        output='screen',
-        namespace = ns,
-        condition=IfCondition(use_fake_hardware)
-    )
-    return LaunchDescription(declared_arguments + [right_gui_node, left_gui_node, right_mock_dbc_node, left_mock_dbc_node])
+    
+    nodes = [
+        right_gui_node,
+        left_gui_node
+    ]
+    
+    return LaunchDescription(declared_arguments + nodes)
