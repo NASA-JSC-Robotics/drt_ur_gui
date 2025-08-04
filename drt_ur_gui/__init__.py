f6d998ec9e1b54130e4a4a5d45bb0ab49b6ab8ce
Mike Tobia <michael.e.tobia@nasa.gov>
2025-08-04 15:21:28 -0500

    Removed __init__.py import * statements and changed to explicit msg/srv module imports. Not sure I need anything in __init__.py, added a TODO for looking into that

--- drt_ur_gui/__init__.py
+++ drt_ur_gui/__init__.py
@@ -1,26 +1,40 @@
 from ur_dashboard_msgs.msg import ProgramState, RobotMode, SafetyMode
-from ur_dashboard_msgs.srv import *
-from std_srvs.srv import *
+from ur_dashboard_msgs.srv import AddToLog, GetLoadedProgram, GetRobotMode, GetSafetyMode, Load, Popup, IsProgramRunning, IsProgramRunning, IsProgramSaved, GetProgramState, RawRequest
+from std_srvs.srv import Trigger
 
 
 def denumerate_ros_msg_type(msg_type):
     return {getattr(msg_type, name): name for name in dir(msg_type) if not name.startswith('_') and isinstance(getattr(msg_type, name), int)}
 
+# ROBOT_MODES
+# 0: 'DISCONNECTED'
+# 1: 'CONFIRM_SAFETY'
+# 2: 'BOOTING'
+# 3: 'POWER_OFF'
+# 4: 'POWER_ON'
+# 5: 'IDLE'
+# 6: 'BACKDRIVE'
+# 7: 'RUNNING'
+# 8: UPDATING_FIRMWARE
 ROBOT_MODES = denumerate_ros_msg_type(RobotMode)
-'''
-ROBOT_MODES
-0: 'DISCONNECTED'
-1: 'CONFIRM_SAFETY'
-2: 'BOOTING'
-3: 'POWER_OFF'
-4: 'POWER_ON'
-5: 'IDLE'
-6: 'BACKDRIVE'
-7: 'RUNNING'
-8: UPDATING_FIRMWARE
-'''
 
+# SAFETY_MODES
+# 1: 'NORMAL'
+# 2: 'REDUCED'
+# 3: 'PROTECTIVE_STOP'
+# 4: 'RECOVERY'
+# 5: 'SAFEGUARD_STOP'
+# 6: 'SYSTEM_EMERGENCY_STOP'
+# 7: 'ROBOT_EMERGENCY_STOP'
+# 8: 'VIOLATION'
+# 9: 'FAULT'
+# 10: 'VALIDATE_JOINT_ID'
+# 11: 'UNDEFINED_SAFETY_MODE'
+# 12: 'AUTOMATIC_MODE_SAFEGUARD_STOP'
+# 13: 'SYSTEM_THREE_POSITION_ENABLING_STOP'
 SAFETY_MODES = denumerate_ros_msg_type(SafetyMode)
+
+# TODO: Convert SERVICES list to config/services.yaml
 SERVICES= [
             {
                 'name':'add_to_log',
