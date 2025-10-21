a4f7d3b532249a83ebec204a68e6dd5ac800171c
Mike Tobia <michael.e.tobia@nasa.gov>
2025-10-21 13:41:44 -0500

    License info

--- drt_ur_gui/__init__.py
+++ drt_ur_gui/__init__.py
@@ -1,10 +1,25 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2025, United States Government, as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+#
+# All rights reserved.
+#
+# This software is licensed under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with the
+# License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
 from ur_dashboard_msgs.msg import ProgramState, RobotMode, SafetyMode
 from ur_dashboard_msgs.srv import AddToLog, GetLoadedProgram, GetRobotMode, GetSafetyMode, Load, Popup, IsProgramRunning, IsProgramRunning, IsProgramSaved, GetProgramState, RawRequest
 from std_srvs.srv import Trigger
-
-
-# class ServiceWrapper:
-
 
 def denumerate_ros_msg_type(msg_type):
     """Converts enumerated ROS 2 message types to {integer_value:name} dictonaries
